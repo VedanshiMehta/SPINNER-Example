e9aecc04937193e2b55e2cd9bfe4104365b03dc5
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-01-27 17:59:33 +0530

    Add project files.

---
+++ SPINNEREx/KeyVP.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPINNEREx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    class KeyVP : AppCompatActivity
+    {
+        private Spinner mySpinner;
+        private List<KeyValuePair<String, String>> friends;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.keypairL);
+
+
+            mySpinner = FindViewById<Spinner>(Resource.Id.spinner1);
+        
+
+            friends = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("Ross","29 Years"),
+                new KeyValuePair<string, string>("Rachel","25 Years"),
+                new KeyValuePair<string, string>("Chandler", "25 Years"),
+                new KeyValuePair<string, string>("Monica", "20 Years"),
+                new KeyValuePair<string, string>("Joey", "29 Years"),
+                new KeyValuePair<string, string>("Pheobe", "30 Years"),
+         
+
+            };
+
+            List<string> friendage = new List<string>();
+            foreach (var item in friends)
+                friendage.Add(item.Key);
+
+            ArrayAdapter adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerItem, friendage);
+
+            mySpinner.Adapter = adapter;
+
+            mySpinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+
+
+
+
+        }
+
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            mySpinner = (Spinner)sender;
+            string t = string.Format("Hey {0} age is {1} ",mySpinner.GetItemAtPosition(e.Position),friends[e.Position].Value);
+            Toast.MakeText(Application.Context,t,ToastLength.Short).Show();
+
+        }
+    }
+}
---
+++ SPINNEREx/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace SPINNEREx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public Spinner mySpinner;
+       // private TextView myTextView;
+        private ArrayAdapter myadpater;
+        private Button myButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            mySpinner = FindViewById<Spinner>(Resource.Id.spinner);
+            mySpinner.ItemSelected += new System.EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_itemSelected);
+            myButton = FindViewById<Button>(Resource.Id.button1);
+            myButton.Click += MyButton_Click;
+            myadpater = ArrayAdapter.CreateFromResource(this, Resource.Array.sixfriend, Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            myadpater.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            mySpinner.Adapter = myadpater;
+            
+        }
+
+        private void MyButton_Click(object sender, EventArgs e)
+        {
+            Intent i = new Intent(Application.Context, typeof(KeyVP));
+            StartActivity(i);
+        }
+
+        private void spinner_itemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            mySpinner = (Spinner)sender;
+            //string t = string.Format("Hey {0}", mySpinner.GetItemAtPosition(e.Position));
+           // Toast.MakeText(this, t, ToastLength.Short).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
